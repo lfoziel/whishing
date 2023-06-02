3979712de3e83e7ad688a1bdc0ad734fd4bed609
lfoziel <49812353+lfoziel@users.noreply.github.com>
2023-06-02 11:31:06 +0300

    Update whishing.ps1

--- whishing.ps1
+++ whishing.ps1
@@ -1,4 +1,4 @@
 #PowerShell based, execute it all at once, Ogi :)
-$base64Code = "RnJlZXdlbGwsIGJyb3RoZXIsIEkgd2lzaCB5b3UgYSBsb3Qgb2Ygc3VjY2VzcywgeW91IGRlc2VydmUgaXQhCkZvciB0aGUgYmVzdCBtYW5hZ2VyLg=="
+$base64Code = "RmFyZXdlbGwsIGJyb3RoZXIsIHdpc2ggeW91IGEgbG90IG9mIHN1Y2Nlc3MsIHlvdSBkZXNlcnZlIGl0IQpGb3IgdGhlIGJlc3QgbWFuYWdlci4KT2dpLg=="
 $decodedText = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($base64Code))
 Write-Host $decodedText
